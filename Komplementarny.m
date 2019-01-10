6ec32fb46e97e208408e78018d012d4b21ee1760
DanielRobotyMobilne <46224476+DanielRobotyMobilne@users.noreply.github.com>
2019-01-10 14:53:54 +0100

    Update Komplementarny.m

--- Komplementarny.m
+++ Komplementarny.m
@@ -1,31 +1,37 @@
 clear all 
 %% wczytanie danych
 data = importdata('data.txt'); 
-ax = data(:,1); 
-ay = data(:,2); 
-az = data(:,3);
-gx = data(:,4);
-gy = data(:,5);
-gz = data(:,6);
+ay = data(:,1);                  
+az = data(:,2); 
+ax = data(:,3);
+gy = data(:,4)*250/32768; 
+gz = data(:,5)*250/32768;
+gx = data(:,6)*250/32768;
 t = data(:,7)/1000000;
 dt = t(2:end)-t(1:end-1);
 dlugosc = length(data);
    
 %% filtr komplementarny
-kat_akcel = atan2(ay,az) * 180/pi;
-T = 0.1;
+
+kat_akcel = zeros(length(data),1); 
+for i = 1:length(data);
+    kat_akcel(i) = atan2(sqrt(ax(i)^2 + ay(i)^2), az(i)) * 180/pi;
+end
+
+T = 0.3;
 
 kat_zyro = zeros(dlugosc,1);
 kat_filtr = zeros(dlugosc,1);
     
     for i=2:dlugosc
+        
         if i==2
-            kat_zyro(1) = gx(1)*dt(1);
+            kat_zyro(1) = gz(1)*dt(1);
             p = T./(dt(1)+T);
             kat_filtr(1) = (1-p).*kat_akcel(1) + p.*kat_zyro(1);
         end
         
-        kat_zyro(i) = kat_zyro(i-1) + gx(i)*dt(i-1);
+        kat_zyro(i) = kat_zyro(i-1) + gz(i)*dt(i-1);
         p = T./(dt(i-1) +T);
         kat_filtr(i) = p.*kat_filtr(i-1) + (1-p).*kat_akcel(i) +p.*(kat_zyro(i)-kat_zyro(i-1));
         
@@ -34,5 +40,5 @@ kat_filtr = zeros(dlugosc,1);
 %% wykres 
 plot(t, kat_zyro, 'b', t, kat_akcel, 'r', t, kat_filtr, 'g')
 legend('kat z zyroskopu', 'kat z akcelerometru', 'kat po filtracji')
-xlabel('t')
-ylabel('\phi')
+xlabel('t [s]')
+ylabel('\psi [°]')
